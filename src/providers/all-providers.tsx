d01df6ab9fa9e99714b72e02051a3347b1a13b7e
Aymenbachiri <aymen.bachiri99@gmail.com>
2026-06-14 14:53:00 +0200

    add generateStaticParams

--- src/providers/all-providers.tsx
+++ src/providers/all-providers.tsx
@@ -1,16 +1,23 @@
 import { DirectionProvider } from "@/components/ui/direction";
+import { LocaleType } from "@/i18n/navigation";
 import { ThemeProvider } from "@/providers/theme-provider";
 import { NextIntlClientProvider } from "next-intl";
 
 type AllProvidersProps = {
   children: React.ReactNode;
+  dir: "ltr" | "rtl";
+  locale: LocaleType;
 };
 
-export default function AllProviders({ children }: AllProvidersProps) {
+export function AllProviders({
+  children,
+  dir,
+  locale,
+}: AllProvidersProps): React.JSX.Element {
   return (
-    <NextIntlClientProvider>
+    <NextIntlClientProvider locale={locale}>
       <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-        <DirectionProvider direction="rtl">{children}</DirectionProvider>
+        <DirectionProvider direction={dir}>{children}</DirectionProvider>
       </ThemeProvider>
     </NextIntlClientProvider>
   );
